f87b0e98a9b0c31c3121be388e3b2ba6249ace73
wilkinsonbrian <wilkibrian@gmail.com>
2020-02-02 14:01:38 -0500

    Added Account class Test file and chaining demo

---
+++ src/Chaining.java
@@ -0,0 +1,13 @@
+public class Chaining {
+
+    private int variable1;
+    private int variable2;
+    private int variable3;
+
+    public Chaining(int v1, int v2, int v3) {
+
+        this.variable1 = v1;
+        this.variable2 = v2;
+        this.variable3 = v3;
+    }
+}
